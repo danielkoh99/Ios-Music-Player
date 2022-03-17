d1ffd02d2b0fdcdb227c0b3b731f7f5efa40fb71
rasztafasz <82089101+rasztafasz@users.noreply.github.com>
2022-03-17 17:23:44 +0100

    Initial Commit

---
+++ mp3player/mp3playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mp3playerApp.swift
+//  mp3player
+//
+//  Created by Dani on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct mp3playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
